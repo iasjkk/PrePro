8390389d1b910efd157ff110f69530a67d99e0b6
iasjkk <iasjkk@gmail.com>
2020-03-21 19:30:49 +0530

    data_added

--- dataframe_handler.py
+++ dataframe_handler.py
@@ -3,8 +3,14 @@ import pandas as pd
 from pathlib import Path
 
 
+def process():
+	# write something 
+	pass
+
+
 if __name__ == '__main__':
-	root_path = Path('Main_path')
-	data = pd.read(f'{root_path}data.csv')
+	root_path = f'/home/jitendra/Code/PrePro/Data/' #Path(f'')
+	data = pd.read_csv(f'{root_path}train.csv')
+	print(data)
 	### script for prepocessing ###
-	data.to_csv(f'{root_path}mod_data.csv', index=False)	
+	# data.to_csv(f'{root_path}mod_data.csv', index=False)	
